07960e832c5cfd6fafb11ed01bc2bc2297796be2
Patrick Narciso <patrick.ncsilva@gmail.com>
2019-10-20 10:42:11 -0300

    feat(Uikit/Tabs): adiciona component Tabs

---
+++ src/components/Uikit/Tabs/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Tabs = ({ tab1Title, tab2Title, tab1Content, tab2Content }) => {
+  return (
+    <div style={{ margin: '25px' }} className="a-tabs a-tabs--horizontal">
+      <input className="a-tabs__item" type="radio" id="tab21" name="tabs2" defaultChecked />
+      <label className="a-tabs__label" htmlFor="tab21">
+        <i className="a-icon a-icon--bank a-icon--size-small a-icon--space-right a-tabs__icon" />
+        <span className="a-tabs__text">Cadastro</span>
+      </label>
+      <div className="a-tabs__content">
+        <h2 className="a-title--small">{tab1Title}</h2>
+        {tab1Content}
+      </div>
+      <input className="a-tabs__item" type="radio" id="tab22" name="tabs2" />
+      <label className="a-tabs__label" htmlFor="tab22">
+        <i className="a-icon a-icon--transactions a-icon--size-small a-icon--space-right a-tabs__icon" />
+        <span className="a-tabs__text">Transações</span>
+      </label>
+      <div className="a-tabs__content">
+        <h2 className="a-title--medium">{tab2Title}</h2>
+        {tab2Content}
+      </div>
+      <div className="a-tabs__border" />
+    </div>
+  );
+};
+
+export default Tabs;
--- src/redux/modules/transactions.js
+++ src/redux/modules/transactions.js
@@ -2,10 +2,47 @@ import { createReducer } from 'utils/redux-utils';
 
 // Constants
 
+const CREATE_TRANSACTION = 'CREATE_TRANSACTION';
+const INCREASE_BALANCE = 'INCREASE_BALANCE';
+const DECREASE_BALANCE = 'DECREASE_BALANCE';
+
 // Action Creators
+
+export const createTransaction = transaction => ({
+  type: CREATE_TRANSACTION,
+  payload: transaction,
+});
+
+export const increaseBalance = transactionValue => ({
+  type: INCREASE_BALANCE,
+  payload: transactionValue,
+});
+
+export const decreaseBalance = transactionValue => ({
+  type: DECREASE_BALANCE,
+  payload: transactionValue,
+});
 
 // Initial State
 
-const initialState = {};
+const initialState = {
+  transactions: [],
+  balance: 0,
+};
 
 // Reducer
+
+export default createReducer(initialState, {
+  [CREATE_TRANSACTION]: (state, action) => ({
+    ...state,
+    transactions: state.transactions.concat(action.payload),
+  }),
+  [INCREASE_BALANCE]: (state, action) => ({
+    ...state,
+    balance: parseFloat(state.balance) + parseFloat(action.payload),
+  }),
+  [DECREASE_BALANCE]: (state, action) => ({
+    ...state,
+    balance: parseFloat(state.balance) - parseFloat(action.payload),
+  }),
+});
