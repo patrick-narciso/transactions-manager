6786325f15ce871eb72ff22a8432833fe9c71c5a
Patrick Narciso <patrick.ncsilva@gmail.com>
2019-10-20 13:21:28 -0300

    feat(components/Home): adiciona componente de Table como conteúdo da segunda Tab na Home Page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import './styles.css';
-import { Form, Tabs } from 'components/Uikit';
+import { Form, Tabs, Table } from 'components/Uikit';
 
 const Home = () => {
   const { balance } = useSelector(state => state.transactions);
 
   return (
     <div className="container">
-      <Tabs tab1Title={`Saldo: R$ ${balance.toLocaleString('pt-BR')}`} tab1Content={<Form />} tab2Title="Transações" />
+      <Tabs
+        tab1Title={`Saldo: R$ ${balance.toLocaleString('pt-BR')}`}
+        tab1Content={<Form />}
+        tab2Title="Transações"
+        tab2Content={<Table />}
+      />
     </div>
   );
 };
