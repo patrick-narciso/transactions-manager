eb482d9cf58b2808662fe4376d64b2730d4ff82d
Patrick Narciso <patrick.ncsilva@gmail.com>
2019-10-20 15:05:06 -0300

    feat(Intl): adiciona internacionalização do valor de transação e saldo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <div className="container">
       <Tabs
-        tab1Title={`Saldo: R$ ${balance.toLocaleString('pt-BR')}`}
+        tab1Title={`Saldo: ${new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(balance)}`}
         tab1Content={<Form />}
         tab2Title="Transações"
         tab2Content={<Table />}
--- src/components/Uikit/Table/index.js
+++ src/components/Uikit/Table/index.js
@@ -6,6 +6,16 @@ const Table = () => {
   const { transactions } = useSelector(state => state.transactions);
   const sortedTransactions = transactions.sort((a, b) => b.date - a.date);
 
+  const icons = {
+    debit: 'arrow-down icon--negative',
+    credit: 'arrow-up icon--positive',
+  };
+
+  const valueColors = {
+    debit: 'value--negative',
+    credit: 'value--positive',
+  };
+
   return (
     <table>
       <thead>
@@ -20,7 +30,10 @@ const Table = () => {
           sortedTransactions.map(transaction => (
             <tr>
               <td className="a-text--small">{transaction.description}</td>
-              <td className="a-text--small">{transaction.value}</td>
+              <td className={`a-text--small ${valueColors[transaction.type]}`}>
+                <i className={`a-icon--size-small a-icon a-icon--${icons[transaction.type]}`} />
+                {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(transaction.value)}
+              </td>
               <td className="a-text--small">{new Date(transaction.date).toLocaleString()}</td>
             </tr>
           ))}
